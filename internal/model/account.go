3ae656d11e9ed74df2edc314564d1e477fdc0f6f
liyasongAha <liyasong@galaxy-future.com>
2021-12-08 16:57:26 +0800

    style

--- internal/model/account.go
+++ internal/model/account.go
@@ -38,7 +38,7 @@ func (a *Account) AfterFind(tx *gorm.DB) (err error) {
 		res, err := encrypt.AESDecrypt(a.AccountKey+encrypt.AesKeySalt, a.AccountSecret)
 		if err != nil {
 			logs.Logger.Errorf("decrypt sk failed.err: %s", err.Error())
-			return nil
+			return err
 		}
 		a.AccountSecret = res
 	}
